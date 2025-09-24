e9c15c7ae813afdcf9be517705da074cec5067f8
amirmatics <4miralbekord@gmail.com>
2025-09-24 12:04:53 -0700

    fixed randomizer

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,41 @@ function App() {
   const [cards, setCards] = useState(cardsInfo)
   const [index, setIndex] = useState(0)
   console.log(cards)
-
-  const currentCard = cards[index]
+  
   function handleBackClick() {
-    setIndex(index - 1)
+    if(index > 0)
+      setIndex(index - 1)
   }
+  
   function handleForwardClick() {
-    setIndex(index + 1)
+    if(index < cards.length - 1)
+      setIndex(index + 1)
   }
+
+  const shuffle = (array) => {
+    const shuffled = [...array];
+    for (var i = array.length - 1; i > 0; i--) 
+    {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    setCards(shuffled);
+  }
+
   return (
     <>
       <h1 className="title">CS Brain Teaser</h1>
       <p>Here is a set of {cards.length} flashcards to learn some fun and important concepts about Computer Science!</p>
-      <Card front={currentCard.front} back={currentCard.back} difficulty={currentCard.difficulty}/>
+      <Card front={cards[index].front} back={cards[index].back} difficulty={cards[index].difficulty}/>
       <div className="arrow-buttons">
-      {index > 0 && 
         <button className="arrow" onClick={handleBackClick}>
             <span>←</span>
         </button>
-      }
-      {index < cards.length - 1 && 
         <button className="arrow" onClick={handleForwardClick}>
           <span>→</span>
         </button>
-      }
       </div>
-      <button className="random-button">Randomize</button>
+      <button className="random-button" onClick={() => shuffle(cards)}>Randomize</button>
     </>
   )
 }
