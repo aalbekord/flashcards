8f67ed98467e48a5eeabbc46458c2da83c8de160
amirmatics <4miralbekord@gmail.com>
2025-09-30 16:12:43 -0700

    fixed spacebar and arrow keys activating hook while typing answer

--- src/hooks/useOnKeyPress.js
+++ src/hooks/useOnKeyPress.js
@@ -1,9 +1,12 @@
 import { useEffect } from 'react';
 
 export const useOnKeyPress = (callback, targetKey) => {
-
   useEffect(() => {
     const handleKeyPress = (event) => {
+      const activeElement = document.activeElement;
+      if(activeElement.tagName === "INPUT" || activeElement.tagName === "TEXTAREA") {
+        return
+      }
       if (event.key === targetKey) {
         callback(); // Call the callback function
       }
